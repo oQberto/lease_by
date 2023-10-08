e54f968c255cf790fa03e352270ef3218b155c61
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-08 17:23:21 +0300

    feat: fill Amenities & Feature enums test-resources: add data for the tests

--- src/main/java/com/example/lease_by/model/entity/enums/Amenities.java
+++ src/main/java/com/example/lease_by/model/entity/enums/Amenities.java
@@ -1,4 +1,18 @@
 package com.example.lease_by.model.entity.enums;
 
 public enum Amenities {
+    BARS,
+    CAFE,
+    DOG_PARK,
+    GYM,
+    MOVIE_THEATER,
+    POOL,
+    SCHOOL,
+    SHOPPING_CENTRE,
+    ATM,
+    PARKS,
+    PUBLIC_LIBRARY,
+    GROCERY_STORE,
+    HOSPITAL,
+    BUS_STOP,;
 }
--- src/main/java/com/example/lease_by/model/entity/enums/Feature.java
+++ src/main/java/com/example/lease_by/model/entity/enums/Feature.java
@@ -1,4 +1,13 @@
 package com.example.lease_by.model.entity.enums;
 
 public enum Feature {
+    BIKE_ROOM,
+    ELECTRIC_VEHICLE_CHARGER,
+    FITNESS_AREA,
+    MOVIE_ROOM,
+    SAUNA,
+    STORAGE_LOCKERS,
+    SWIMMING_POOL,
+    VIDEO_SURVEILLANCE,
+    SECURITY_24H,;
 }
