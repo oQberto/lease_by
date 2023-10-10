7dd2f1beb447785a1ddfe5a37bd7a6a3c4ea88d6
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-10 16:56:20 +0300

    fix: remove redundant properties

--- src/main/java/com/example/lease_by/mapper/CityMapper.java
+++ src/main/java/com/example/lease_by/mapper/CityMapper.java
@@ -4,7 +4,9 @@ import com.example.lease_by.dto.CityReadDto;
 import com.example.lease_by.model.entity.City;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
 public interface CityMapper {
 
     CityReadDto mapToCityReadDto(City entity);
--- src/main/java/com/example/lease_by/mapper/UserReadMapper.java
+++ src/main/java/com/example/lease_by/mapper/UserReadMapper.java
@@ -4,13 +4,9 @@ import com.example.lease_by.dto.UserReadDto;
 import com.example.lease_by.model.entity.User;
 import org.mapstruct.Mapper;
 
-import static org.mapstruct.InjectionStrategy.CONSTRUCTOR;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
-@Mapper(
-        componentModel = SPRING,
-        injectionStrategy = CONSTRUCTOR
-)
+@Mapper(componentModel = SPRING)
 public interface UserReadMapper {
 
     UserReadDto mapToUserReadDto(User entity);
