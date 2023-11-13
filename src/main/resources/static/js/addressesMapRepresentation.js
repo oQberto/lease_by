f86f920c4fb04b0ace687bbb76805c9f87ecab96
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-11-13 18:22:35 +0300

    feat: create script that shows all rentals on the map feat-UI: create maps UI

---
+++ src/main/resources/static/js/addressesMapRepresentation.js
@@ -0,0 +1,45 @@
+const fullUrl = window.location.href;
+const parts = fullUrl.split('/');
+const cityName = parts[parts.length - 1];
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [53.932193, 27.434136],
+        zoom: 14,
+        controls: ['trafficControl']
+    });
+
+    var clusterer = new ymaps.Clusterer({
+        clusterDisableClickZoom: true,
+        clusterOpenBalloonOnClick: true,
+        preset: 'islands#invertedBlueClusterIcons',
+    });
+
+    // Отправляем AJAX-запрос на сервер, чтобы получить данные геокодирования
+    $.ajax({
+        url: '/api/v1/rentals/geocode/' + cityName,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Обработка данных, полученных от сервера
+            data.forEach(function (point) {
+                var placemark = new ymaps.Placemark([point.latitude, point.longitude], {balloonContent: point.pointName});
+
+                placemark.events.add('click', function (e) {
+                    $('#markerModal').modal('show');
+                    $('#markerModalTitle').text(point.pointName);
+                    // Можете добавить вывод других данных, если необходимо
+                });
+
+                clusterer.add(placemark);
+            });
+
+            myMap.geoObjects.add(clusterer);
+        },
+        error: function (error) {
+            console.log('Ошибка при получении данных геокодирования:', error);
+        }
+    });
+}
